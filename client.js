ebef41a5762d7d0d2ea7b949ade9d261db4bcf35
CodeJuuun <Chomseng.a@gmail.com>
2024-12-29 22:54:45 -0500

    separated connect function into it's own module and connected it to play.js, both file works

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require("net");
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  // receives incoming msg and transmits to user
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  // added error handling event to listen
+  conn.on("error", (error) => {
+    console.log("Connection error: ", error);
+  });
+
+
+  // added event to close connection server when no longer needed
+  conn.on("end", () => {
+    console.log("Connection to server has ended");
+  });
+
+  conn.setEncoding("utf8");
+  
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,32 +1,5 @@
-const net = require("net");
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  // receives incoming msg and transmits to user
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  // added error handling event to listen
-  conn.on("error", (error) => {
-    console.log("Connection error: ", error);
-  });
+const connect = require("./client");
 
 
-  // added event to close connection server when no longer needed
-  conn.on("end", () => {
-    console.log("Connection to server has ended");
-  });
-
-  conn.setEncoding("utf8");
-  
-  console.log("Connecting ...");
-  return conn;
-};
-
-
+console.log("Connecting ...");
 connect();
